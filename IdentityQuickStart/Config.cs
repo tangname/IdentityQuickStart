42af28ea244481f9a563daa03a23e69f44b6cd7b
tangname <249195807@qq.com>
2018-08-24 15:01:45 +0800

    添加项目文件。

---
+++ IdentityQuickStart/Config.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityQuickStart
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+               new ApiResource("api1", "测试API资源")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            var list = new List<Client>();
+
+            var client = new Client
+            {
+                ClientId = "client",
+                //非交互用户，使用Clientid/Secret认证
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                //认证的Secret
+                ClientSecrets ={
+                    new Secret("secret".Sha256())
+                },
+                //客户端要访问的作用域(Api)
+                AllowedScopes = { "api1" }
+            };
+
+            list.Add(client);
+
+            return list;
+        }
+    }
+}
---
+++ QuickStartWebApi/Controllers/IdentityController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickStartWebApi.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var users = from claim in User.Claims
+                        select new
+                        {
+                            claim.Type,
+                            claim.Value
+                        };
+
+            return new JsonResult(users);
+        }
+    }
+}
