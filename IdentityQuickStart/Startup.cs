50e29cf4e540e044c86f30dcfff2149d319c0b95
tangname <249195807@qq.com>
2018-08-31 17:05:29 +0800

    add openid connect authentication

--- IdentityQuickStart/Startup.cs
+++ IdentityQuickStart/Startup.cs
@@ -15,6 +15,8 @@ namespace IdentityQuickStart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             //IdentityServer配置in-memory存储,键(Keys),客户端和资源，测试用户
             services.AddIdentityServer()
                     .AddDeveloperSigningCredential()
@@ -33,10 +35,9 @@ namespace IdentityQuickStart
 
             app.UseIdentityServer();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            //添加MVC中间件
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
